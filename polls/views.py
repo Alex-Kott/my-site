9f577be7e28e03cae86de05c882141472ba3856d
AlexKott <alexey.kott@gmail.com>
2017-08-30 19:04:46 +0300

    отправляется письмо на почту

--- polls/views.py
+++ polls/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.utils import timezone
-
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import re
 
 from .models import Posts
 from .models import Users
 
 def index(request):
-	
 	posts = Posts.objects.all()
 	context = { 'posts' : posts}
 	return render(request, 'polls/index.html', context)
@@ -27,20 +29,58 @@ def signup(request):
 	form = request.POST
 	print(form)
 	try:
-		u = Users(login = form['login'], password = form['password'], date = timezone.now())
-		u.save()
+		user = Users.objects.get(login = login)
+		context['error'] = 'user_exist'
+		return render(request, 'polls/error.html', context)
 	except:
-		print("User registration error")
+		try:
+			u = Users(login = form['login'], password = form['password'], date = timezone.now())
+			u.save()
+			# test_django_app:qazwsx123
+			if is_email(form['login']):
+				send_mail(request)
+		except Exception as e:
+			print(str(e))
 	return redirect("/")
+
+def send_mail(request):
+	print(request)
+	form = request.POST
+	fromaddr = "test_django_app@mail.ru"
+	toaddr = form['login']
+	mypass = "qazwsx123"
+	 
+	msg = MIMEMultipart()
+	msg['From'] = fromaddr
+	msg['To'] = toaddr
+	msg['Subject'] = "Регистрация"
+	 
+	body = "Вы зарегистрировались в нашем блоге. Ваш пароль: {}".format(form['password'])
+	msg.attach(MIMEText(body, 'plain'))
+	 
+	server = smtplib.SMTP('smtp.mail.ru', 587)
+	server.starttls()
+	server.login(fromaddr, mypass)
+	text = msg.as_string()
+	try:
+		server.sendmail(fromaddr, toaddr, text)
+	except:
+		print("Mail send error")
+	server.quit()
+
+def is_email(email):
+	flag = re.findall(r'^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$', email)
+	if len(flag) == 1:
+		return True
+	else:
+		return False
 
 def auth(request):
 	form = request.POST
 	login = form['login']
 	password = form['password']
-	print(login)
 	context = {}
 	try:
-
 		user = Users.objects.get(login = login)
 	except Users.DoesNotExist:
 
@@ -53,3 +93,4 @@ def auth(request):
 		return render(request, 'polls/error.html', context)
 	
 	return redirect("/")
+
